179127ecaf274ee9629a705f9c5b3b5b2ccb7d4e
Joe-CDN <35740892+Joe-CDN@users.noreply.github.com>
2021-10-05 20:59:37 -0400

    Second

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+}
